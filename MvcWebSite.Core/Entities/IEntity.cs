fe62154ddc6bc972618ee681e824226f6ac6fcc3
OrhanKaynak <kaynakorhan354@gmail.com>
2026-06-25 23:01:50 +0300

    Add project files.

---
+++ MvcWebSite.Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcWebSite.Core.Entities
+{
+    public interface IEntity
+    {
+        public int Id { get; set; }
+    }
+}
